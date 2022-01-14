166d4bb8e6f731e268e23c3082003737f22b0e9e
teppei-mcbi <teppei.nishimura@mcbi.com.au>
2022-01-14 23:13:12 +1030

    set up jest

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-const world1 = 'world';
+// const world1 = 'world';
 
-export function hello(world: string = world1): string {
-  return `Hello ${world}! `;
+// export function hello(world: string = world1): string {
+//   return `Hello ${world}! `;
+// }
+
+// const result = hello();
+// console.log(`result: ${result}`)
+
+export function sum(a, b) {
+    return a + b;
 }
 
-const result = hello();
-console.log(`result: ${result}`)
+
+// module.exports = sum;
